7ab9e7b23b8a3d5e7a0ea7389b6d062dfb9fc37a
zyan <zyan@ML350p>
2026-01-09 14:40:34 -0500

    Initial Push All Fri Jan  9 14:40:34 EST 2026

---
+++ Nextseq-archerdx_CMP/251217_NDX551323_0003_AHTC5YBGYX.sh
@@ -0,0 +1,12 @@
+# 251217_NDX551323_0003_AHTC5YBGYX
+
+nextseqdx=nextseq33
+project=251217_NDX551323_0003_AHTC5YBGYX
+backup=/mnt/win-hp3_1/NGS_Cambridge/NextSeq_merged_fastq
+dxrun="null"
+sharef=/mnt/win-hp1
+cpdir=Nextseq-archerdx_CMP
+
+nohup bash /host/DATA/ZYAN/WORK/NEXT-GEN-SEQ/codes/Nextseq-archerdx_CMP.sh $nextseqdx $project $backup "$dxrun" $sharef $cpdir 2>&1 | nohup tee -a tmp/$project.nohup
+
+

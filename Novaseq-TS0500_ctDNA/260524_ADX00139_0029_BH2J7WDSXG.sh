66d5329fe6250bbcde444e274564e9fa37eac046
zyan <zyan@ML350p>
2026-05-27 10:23:49 -0400

    Initial Push All Wed May 27 10:23:49 EDT 2026

---
+++ Novaseq-TS0500_ctDNA/260524_ADX00139_0029_BH2J7WDSXG.sh
@@ -0,0 +1,6 @@
+#260524_ADX00139_0029_BH2J7WDSXG
+novaseqdx=Dragen-RUO-NAS
+run=260524_ADX00139_0029_BH2J7WDSXG
+sharef=/mnt/win-hp1		#share folder /mnt/win-hp or /mnt/win-hp3_1/NGS_Cambridge etc.
+
+nohup bash /host/DATA/ZYAN/WORK/NEXT-GEN-SEQ/codes/Novaseq-TS0500_ctDNA-startdragen.sh $novaseqdx $run $sharef 2>&1 | nohup tee -a tmp/$run.nohup
